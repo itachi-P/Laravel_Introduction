5f26cbee3ce97ecd44da2340af2f9f2647665d18
itachi-P <itachip38@gmail.com>
2019-09-01 00:23:45 +0900

    :whale: BB3-4 createing a layout

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -6,10 +6,3 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
-/*
-Route::get('hello', function() {
-    
-});
-*/
-Route::get('singleaction', 'SingleActionController');
-
