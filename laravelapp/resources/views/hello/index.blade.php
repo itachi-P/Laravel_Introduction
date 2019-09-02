8345b242f620ef6b95c3711bacfa654f229233a4
itachi-P <itachip38@gmail.com>
2019-09-02 22:55:57 +0900

    :cookie: BB4-4 read&write cookies(p.159)

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -10,6 +10,9 @@
 
 @section('content')
     <p>{{$msg}}</p>
+    @foreach ($errors as $error)
+        {{$error}}
+    @endforeach
     @if (count($errors) > 0)
         <p>入力に問題があります。再入力して下さい。</p>
     @endif
@@ -17,22 +20,13 @@
     <table>
         <form action="/hello" method="POST">
             {{ csrf_field() }}
-            @if ($errors->has('name'))
-                <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+            @if ($errors->has('msg'))
+                <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
             @endif
-            <tr><th>name: </th><td><input type="text" name="name"
-                value="{{old('name')}}"></td></tr>
-            @if ($errors->has('mail'))
-                <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
-            @endif
-            <tr><th>mail: </th>><td><input type="text" name="mail"
-                value="{{old('mail')}}"></td></tr>
-            @if ($errors->has('age'))
-                <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
-            @endif
-            <tr><th>age: </th>><td><input type="text" name="age"
-                value="{{old('age')}}"></td></tr>
-            <tr><th></th>><td><input type="submit" value="send"></td></tr>
+
+            <tr><th>Message: </th><td><input type="text" name="msg"
+                value="{{old('msg')}}"></td></tr>
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
         </form>
     </table>
 @endsection
