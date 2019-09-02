3be3afae39dc41a49cf061dfd07ff34f1d0086c5
itachi-P <itachip38@gmail.com>
2019-09-02 11:30:28 +0900

    :speak_no_evil: BlueBook 4-1 Use Middleware p.113

---
+++ laravelapp/app/Http/Middleware/HelloMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HelloMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $content = $response -> content ( ) ;
+        
+        # 正規表現パターンでHTML中の<middlewara>タグの中身からリンクを自動生成する
+        $pattern = '/<middleware>(.*)<\/middleware>/i';
+        $replace = '<a href="http://$1">$1</a>';
+        $content = preg_replace($pattern, $replace, $content);
+        $response -> setContent($content);
+        return $response;
+    }
+}
