cd93e4c8e4c36bb898a1467fe7b3058516f9ba6a
itachi-P <itachip38@gmail.com>
2019-09-02 12:01:12 +0900

    :tongue: BB4-1 modifing response by middleware

--- laravelapp/app/Http/Middleware/HelloMiddleware.php
+++ laravelapp/app/Http/Middleware/HelloMiddleware.php
@@ -10,7 +10,7 @@ class HelloMiddleware
         $response = $next($request);
         $content = $response -> content ( ) ;
         
-        # 正規表現パターンでHTML中の<middlewara>タグの中身からリンクを自動生成する
+        # 正規表現による置換でHTML中の<middlewara>タグの中身からリンクを自動生成する
         $pattern = '/<middleware>(.*)<\/middleware>/i';
         $replace = '<a href="http://$1">$1</a>';
         $content = preg_replace($pattern, $replace, $content);
