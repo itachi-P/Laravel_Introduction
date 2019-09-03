b9626823f94004137c47063ba538ef1288eecf56
itachi-P <itachip38@gmail.com>
2019-09-03 22:05:49 +0900

    orderBy p.205

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -12,7 +12,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
         return view('hello.index', ['items' => $items]);
     }
 
@@ -75,10 +75,10 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {        
-        $name = $request->name;
+        $min = $request->min;
+        $max = $request->max;
         $items = DB::table('people')
-            ->where('name', 'like', '%' . $name . '%')
-            ->orWhere('mail', 'like', '%' . $name . '%')
+            ->whereRaw('age >= ? and age <= ?', [$min, $max])
             ->get();
         return view('hello.show', ['items' => $items]);
     }
