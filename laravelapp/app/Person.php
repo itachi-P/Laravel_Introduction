91e8760529030a2c5aa126e3d9c6554eca0a9aa5
itachi-P <itachip38@gmail.com>
2019-09-04 22:19:18 +0900

    BB 6-2 making Scope class.

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -23,5 +25,12 @@ class Person extends Model
     public function scopeAgeLessThan($query, $n)
     {
         return $query->where('age', '<=', $n);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ScopePerson);
     }
 }
