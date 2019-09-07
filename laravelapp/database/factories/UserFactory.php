454e32ecd35aec924ec391530e2ef01cf2e322f5
itachi-P <itachip38@gmail.com>
2019-09-07 13:42:17 +0900

    List7-38 Successed

--- laravelapp/database/factories/UserFactory.php
+++ laravelapp/database/factories/UserFactory.php
@@ -3,7 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
 use Illuminate\Support\Str;
-use Faker\Generator;
+use Faker\Generator as Faker;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,20 +16,12 @@ use Faker\Generator;
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-    ];
-});
-
-$factory->define(App\Person::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'mail' => $faker->safeEmail,
-        'age' => random_int(1, 99),
     ];
 });
--- laravelapp/tests/Feature/HelloTest.php
+++ laravelapp/tests/Feature/HelloTest.php
@@ -5,28 +5,21 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use App\User;
 
 class HelloTest extends TestCase
 {    
-    use DatabaseMigrations;
-
     public function testHello()
     {
         $this->assertTrue(true);
 
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $arr = [];
+        $this->assertEmpty($arr);
 
-        $response = $this->get('/hello');
-        $response->assertStatus(302);
+        $msg = "Hello";
+        $this->assertEquals('Hello', $msg);
 
-        // Illuminate\Database\QueryException: Database (database\database_test.sqlite) does not exist.
-        $user = factory(User::class)->create();
-        $response = $this->actingAs($user)->get('/hello');
-        $response->assertStatus(200);
+        $n = random_int(0, 100);
+        $this->assertLessThan(100, $n);
 
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
     }
 }
